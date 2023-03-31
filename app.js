c926332174646401315b58ca50525a0cfb7f58fc
devWil01 <123305625+devWil01@users.noreply.github.com>
2023-03-31 11:56:50 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+var btn = document.querySelector(".no");
+var position
+btn.addEventListener("click", function() {
+position ? (position = 0) : (position = 150);
+btn.style.transform = `translate(${position}px,0px)`;
+btn.style.transition = "all 0.2s ease";
+});
+
+var btn = document.querySelector(".no");
+var position
+btn.addEventListener("mouseover", function() {
+position ? (position = 0) : (position = 150);
+btn.style.transform = `translate(${position}px,0px)`;
+btn.style.transition = "all 0.2s ease";
+});
+
+const sim = document.getElementById('yes');
+
+sim.addEventListener("click", () => {
+
+let timerInterval
+Swal.fire({
+  title: 'Obrigado 😍',
+  html: 'Prometo lhe fazer feliz. 💘',
+  timer: 2000,
+  timerProgressBar: true,
+  didOpen: () => {
+    Swal.showLoading()
+    const b = Swal.getHtmlContainer().querySelector('b')
+    timerInterval = setInterval(() => {
+      b.textContent = Swal.getTimerLeft()
+    }, 100)
+  },
+  willClose: () => {
+    clearInterval(timerInterval)
+  }
+}).then((result) => {
+  /* Read more about handling dismissals below */
+  if (result.dismiss === Swal.DismissReason.timer) {
+    console.log('I was closed by the timer')
+  }
+})
+
+});
+
