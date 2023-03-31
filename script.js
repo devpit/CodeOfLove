dcbe8aef0e4d5f7977464f6dc868d4fd03c5a965
devWil01 <123305625+devWil01@users.noreply.github.com>
2023-03-31 10:54:32 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var position
 btn.addEventListener("click", function() {
 position ? (position = 0) : (position = 150);
 btn.style.transform = `translate(${position}px,0px)`;
-btn.style.transition = "all 0.3s ease";
+btn.style.transition = "all 0.2s ease";
 });
 
 var btn = document.querySelector(".no");
@@ -11,9 +11,35 @@ var position
 btn.addEventListener("mouseover", function() {
 position ? (position = 0) : (position = 150);
 btn.style.transform = `translate(${position}px,0px)`;
-btn.style.transition = "all 0.3s ease";
+btn.style.transition = "all 0.2s ease";
 });
 
-function tanks() {
-alert("tanks :)")
-}  
+const sim = document.getElementById('yes');
+
+sim.addEventListener("click", () => {
+
+let timerInterval
+Swal.fire({
+  title: 'Obrigado 😍',
+  html: 'Prometo lhe fazer feliz. 💘',
+  timer: 2000,
+  timerProgressBar: true,
+  didOpen: () => {
+    Swal.showLoading()
+    const b = Swal.getHtmlContainer().querySelector('b')
+    timerInterval = setInterval(() => {
+      b.textContent = Swal.getTimerLeft()
+    }, 100)
+  },
+  willClose: () => {
+    clearInterval(timerInterval)
+  }
+}).then((result) => {
+  /* Read more about handling dismissals below */
+  if (result.dismiss === Swal.DismissReason.timer) {
+    console.log('I was closed by the timer')
+  }
+})
+
+});
+
