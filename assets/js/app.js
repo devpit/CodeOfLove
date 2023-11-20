f1749a9bb1c123e432e6e46f418b066dea1c9ba3
Pit traquinagens <123305625+devpit@users.noreply.github.com>
2023-11-20 19:58:03 -0300

    Create app.js

---
+++ assets/js/app.js
@@ -0,0 +1,59 @@
+var btn = document.querySelector(".no");
+var position = 0;
+var isAnimating = false;
+
+btn.addEventListener("click", function() {
+    if (!isAnimating) {
+        isAnimating = true;
+        position = position === 0 ? 150 : 0;
+        btn.style.transform = `translate(0px, ${position}px)`;
+        btn.style.transition = "all 0.2s ease";
+      
+        setTimeout(function() {
+            isAnimating = false;
+        }, 200);
+    }
+});
+
+btn.addEventListener("mouseover", function() {
+    if (!isAnimating) {
+        isAnimating = true;
+        position = position === 0 ? 150 : 0;
+        btn.style.transform = `translate(0px, ${position}px)`;
+        btn.style.transition = "all 0.2s ease";
+
+        setTimeout(function() {
+            isAnimating = false;
+        }, 200);
+    }
+});
+
+
+const sim = document.getElementById('yes');
+
+sim.addEventListener("click", () => {
+
+let timerInterval
+Swal.fire({
+  title: 'Obrigado 😍',
+  html: 'Prometo lhe fazer feliz. 💘',
+  timer: 2000,
+  timerProgressBar: true,
+  didOpen: () => {
+    Swal.showLoading()
+    const b = Swal.getHtmlContainer().querySelector('b')
+    timerInterval = setInterval(() => {
+      b.textContent = Swal.getTimerLeft()
+    }, 100)
+  },
+  willClose: () => {
+    clearInterval(timerInterval)
+  }
+}).then((result) => {
+  if (result.dismiss === Swal.DismissReason.timer) {
+    console.log('I was closed by the timer')
+  }
+})
+
+});
+
